38260e9cd6bd488e37caa217e587e088eaa45361
Sandro Cabrera <cabrerasandro682@gmail.com>
2024-09-25 22:06:06 -0500

    Carga inicial T1

---
+++ src/main/java/pe/edu/cibertec/t1_frontend/T1FrontendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.t1_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1FrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1FrontendApplication.class, args);
+	}
+
+}
